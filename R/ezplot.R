28afbb3b8b031f6894bebf026d1b071a2df4fa60
Aaron Cooley <aaronsama@gmail.com>
2018-01-18 17:10:09 -0700

    Add aggr to ezplot

--- R/ezplot.R
+++ R/ezplot.R
@@ -3,7 +3,10 @@
 #' @description 
 #' Creates a ggplot2 object using greatly simplified syntax
 #' 
-#' @param data (\code{data.frame} or \code{tibble}) Data to be used in plot
+#' @param data (\code{data.frame} | \code{tibble}) Data to be used in plot
+#' @param aggr (\code{function}) (Optional) function created using
+#'   \code{dplyr::summarise}. Must take the form \code{function(x) summarise(x,
+#'   ...)}
 #' @param plot_type \code{character(1)} Type of plot to create. Default is \code{'bar'}
 #' \itemize{
 #'   \item \code{'bar'} Create bar plot using \code{geom_bar()}
@@ -26,15 +29,19 @@
 #' @param facet (Optional) Variable to facet by
 #' 
 #' @export
-ezplot <- function(data, plot_type = "bar", x, y, group = NULL, group_method = "stack", 
+ezplot <- function(data, aggr = NULL, plot_type = "bar", x, y, group = NULL, group_method = "stack", 
                    facet = NULL, palette = NULL) {
   checkmate::assert_subset(plot_type, c("bar", "line", "point", "area"))
+  checkmate::assert_function(aggr, null.ok = TRUE)
   checkmate::assert_subset(x, names(data), empty.ok = FALSE)
-  checkmate::assert_subset(y, names(data), empty.ok = FALSE)
   checkmate::assert_subset(group, names(data))
   checkmate::assert_subset(group_method, c("stack", "dodge", "fill"))
   checkmate::assert_subset(facet, names(data))
   checkmate::assert_character(palette, null.ok = TRUE)
+  
+  if (hasArg("aggr")) {
+    data <- group_by_at(data, .vars = c(x, group, facet)) %>% aggr
+  }
   
   gg <- wrapr::let(
     list(x_ = x,
